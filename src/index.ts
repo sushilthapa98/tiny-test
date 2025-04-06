74403170c448bd7c37d26cac1105ac81d7e1c277
Sushil Thapa <thapasushil6@gmail.com>
2025-04-06 22:22:40 +0545

    add new greet function

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,8 @@ const greet2 = (name: string): string => {
   return `Hello, ${name}!`;
 };
 
+const greet3 = (name: string): string => {
+  return `Hello3, ${name}!`;
+};
+
 console.log(greet('World'));
