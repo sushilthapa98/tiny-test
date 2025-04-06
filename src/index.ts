e43228e6cb23f9c80ca83941a6370c0773a59983
Sushil Thapa <35750778+sushilthapa98@users.noreply.github.com>
2025-04-06 23:23:25 +0545

    fix typo (#8)
    * minor fix
    
    * added ci cd scripts
    
    * update package json
    
    * release v2
    
    * minor fix

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const greet3 = (name: string): string => {
 };
 
 const greet5 = (name: string): string => {
-  return `Hello5, ${name}!`;
+  return `Hello, ${name}!`;
 };
 
 console.log(greet('World'));
