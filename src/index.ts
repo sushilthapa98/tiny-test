6ef2d1e50f4366447a91fb0de8d59ecb6f64d657
Sushil Thapa <35750778+sushilthapa98@users.noreply.github.com>
2025-04-06 18:23:36 +0545

    add greet 2 (#1)

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ const greet = (name: string): string => {
   return `Hello, ${name}!`;
 };
 
+const greet2 = (name: string): string => {
+  return `Hello2, ${name}!`;
+};
+
 console.log(greet('World'));
